fa8744bdb2dd318ab5b4544875b03f9d805eb053
Teresa Fikes <Ardenent1@gmail.com>
2014-10-08 14:38:15 -0500

    Example Expressions
    Just some examples of expressions

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
@@ -1,6 +1,5 @@
 // Expressions
 
-var a = 2;//sets up our variable a and defines it with 2
-var b;
-b = a + 3; //add 3 to a
-console.log(b);
+var yearBorn = 1963;
+var age = 2014 - yearBorn - 1;
+console.log(age);
