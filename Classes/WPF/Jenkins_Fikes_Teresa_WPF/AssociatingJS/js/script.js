e0730fc36f8d4e94f961d37e7cdcc6a927034897
Teresa Fikes <Ardenent1@gmail.com>
2014-10-08 16:47:46 -0500

    Casting
    Treating Strings like Numbers and vice versa

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
@@ -1,8 +1,9 @@
-// Expressions - Concatenating Strings
+//Casting
 
-var a = "6";
-var b = "7";
+var areaCode = 407
+var firstPart = 203;
+var secPart = 4529;
+//(407) 203-4529
 
-var result = a + b;
-console.log(result);
-
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) +"-" + String(secPart);
+console.log(phoneNo);
