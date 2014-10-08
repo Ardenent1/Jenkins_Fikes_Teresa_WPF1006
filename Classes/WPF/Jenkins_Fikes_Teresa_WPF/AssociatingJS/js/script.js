cb84e9e1df35d0f22856a5b2c350ea8afd0d2cbc
Teresa Fikes <Ardenent1@gmail.com>
2014-10-08 14:54:26 -0500

    Arithmetic Operators
    Adding it all up together

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/js/script.js
@@ -1,5 +1,7 @@
-// Expressions
+// Expressions - Arithmetic Operators
 
-var yearBorn = 1963;
-var age = 2014 - yearBorn - 1;
-console.log(age);
+//area of the triangle is half the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
