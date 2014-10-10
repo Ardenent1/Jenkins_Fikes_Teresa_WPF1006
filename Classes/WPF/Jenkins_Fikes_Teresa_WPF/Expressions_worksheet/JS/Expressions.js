dee577c553d68ea87fce2142295f7e9f6488508c
Teresa Fikes <Ardenent1@gmail.com>
2014-10-09 19:28:56 -0500

    Calculating Sparky's Age
    First assignment in calculating Sparky’s age

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/JS/Expressions.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/JS/Expressions.js
@@ -2,5 +2,19 @@
  Teresa Jenkins Fikes
  WPF 1408 Section 01
 Expressions work sheet Week 2
+10-09-2014
  */
 
+
+//How old is Sparky in Dog years based on his actual human age?
+//Alert the use
+alert("Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in dog years.");
+
+alert("If Sparky is 1 in human years, then how old is he in dog years? Let's see.");
+var a = 7; //dogYears
+
+var b = a * 1; //humanYears
+console.log(a);
+
+alert("Sparky is 7 years old in dog years");
+console.log(b)
