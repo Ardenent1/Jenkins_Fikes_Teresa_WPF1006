67a0b3298478011210b1b9c4a559b1da97202c49
Teresa Fikes <Ardenent1@gmail.com>
2014-10-09 23:32:02 -0500

    Grocery Bill
    Budgeting your Grocery money for the year

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/Grocery Bill.js
@@ -0,0 +1,18 @@
+/*
+ Teresa Jenkins Fikes
+ WPF 1408 Section 01
+ Go To training Week 2
+ */
+var groceryTotals = ["Week0", "Week1", "Week2", "Week3", "Week4"];//0,1,2,3
+
+console.log(groceryTotals[1]);
+
+groceryTotals[0]= "Week0";
+groceryTotals[2]= "Week1";
+groceryTotals[3]= "Week2";
+groceryTotals[4]= "Week4";
+
+var num = 1;
+console.log(groceryTotal[num]);
+
+
