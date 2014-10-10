982802c7c395060e4b56ff58206e2470cbf7a52e
Teresa Fikes <Ardenent1@gmail.com>
2014-10-10 10:20:29 -0500

    Discount
    Calculate the discounted price for an item.

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/JS/Discounts.js
@@ -0,0 +1,38 @@
+/*
+ Teresa Jenkins Fikes
+ WPF 1408 Section 01
+ Go To training Week 2
+ */
+alert("Calculate the discounted price for an item");
+
+//Ask the user the original price
+//Create a variable to catch their answer
+
+var originalPrice = prompt("What is the Original Price?");
+console.log(originalPrice);
+
+
+// Ask the user what is the Discount Percentage?
+var discountPercentage =prompt("What is the discount percentage?");
+console.log(discountPercentage);
+
+//Ask the user the description of the item.
+var descriptionOfItem = prompt("Description of item.");
+console.log(descriptionOfItem);
+
+//Price of the item with tax
+var tax = prompt ("What is the tax rate?");
+console.log(tax)
+
+//Price of the item without tax
+var withoutTax =0;
+
+Alert (“Your X was originally $X, but after a X% discount, it is now $X without tax, and $X with tax.”);
+
+var answer = (originalPrice * discountPercentage) (descriptionOfItem discountedPrice * sales percentage);
+console.log(answer);
+
+
+
+
+
