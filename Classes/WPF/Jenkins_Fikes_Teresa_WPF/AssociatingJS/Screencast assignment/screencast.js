05b7e8b37fbf50032e471b2230871fb735769a2e
Teresa Fikes <Ardenent1@gmail.com>
2014-10-10 17:34:16 -0500

    Screencast and prompt
    Screencast and prompt changes

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Screencast assignment/screencast.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Screencast assignment/screencast.js
@@ -3,14 +3,14 @@
  WPF 1408 Section 01
  Go To training Week 2
  */
-alert("Testing 1.2. 3");
 
 //Prompt and Alert
 
 var width =prompt("We are calculating the area of a rectangle. \nPlease enter the width");
-
 var height = prompt("Please enter the height.");
 var area = width * height; //calculates with that info
 //console.log(area); //prints it out to console
-var result = "The area of you rectangle is " + area + " sq feet";
-alert(result);
+
+alert("The area of you rectangle is " + area + " sq feet");
+
+
