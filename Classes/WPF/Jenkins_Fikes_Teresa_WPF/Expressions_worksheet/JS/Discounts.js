73a161831126a6485181de5603a10d5a32569061
Teresa Fikes <Ardenent1@gmail.com>
2014-10-10 16:23:39 -0500

    Discounts
    Using Strings to contact and calculate the discounts

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/JS/Discounts.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/Expressions_worksheet/JS/Discounts.js
@@ -7,30 +7,36 @@ alert("Calculate the discounted price for an item");
 
 //Ask the user the original price
 //Create a variable to catch their answer
-
-var originalPrice = prompt("What is the Original Price?");
+alert("What is the Original Price?");
+var a = originalPrice = prompt("What is the Original Price?");
 console.log(originalPrice);
 
 
-// Ask the user what is the Discount Percentage?
+//Ask the user what is the Discount Percentage?
+alert("What is the discount percentage?");
 var discountPercentage =prompt("What is the discount percentage?");
 console.log(discountPercentage);
 
 //Ask the user the description of the item.
+
+alert("Description of item.");
 var descriptionOfItem = prompt("Description of item.");
 console.log(descriptionOfItem);
 
 //Price of the item with tax
+
+alert("What is the tax rate?");
 var tax = prompt ("What is the tax rate?");
 console.log(tax)
 
 //Price of the item without tax
-var withoutTax =0;
 
-Alert (“Your X was originally $X, but after a X% discount, it is now $X without tax, and $X with tax.”);
+alert("Price without tax.");
+var withoutTax;
 
-var answer = (originalPrice * discountPercentage) (descriptionOfItem discountedPrice * sales percentage);
-console.log(answer);
+alert(“Your descriptionOfItem was originally $originalPrice, but after a discountPercentage% discount, it is now $withoutTax without tax, and $tax with tax.”);
+
+var answer = "(" = String(originalPrice) + "*" + String(discountPercentage) + "=" + String(PercentageAmount) + "-" + String(originalPrice) + "*" + String(salesTaxPercentage) + "=" + String(tax) + "+" + String(discountedPrice);
 
 
 
