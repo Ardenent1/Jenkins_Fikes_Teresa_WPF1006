11254e605aba77d096f268dd7407c8f5385f7b0d
Teresa Fikes <Ardenent1@gmail.com>
2014-10-11 14:22:15 -0500

    Personal
    Personal calculation for Mary’s gas expenses for the trip here

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Personal/Personal.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Personal/Personal.js
@@ -4,5 +4,24 @@
  Go To training Week 2
  */
 
-//alert("Testing 1.2. 3");
+//alert("How much money does Mary need for gas to visit me?");
+//Create a js file that will tell us how much money Mary needs for her trip
+
+
+
+alert("Welcome "+Mary+"!  Let's go ahead and figure out the cost of gas to get here." );
+
+//Ask the user what is the mpg of her car?
+
+var mpg = prompt("What is the mpg of your car?");
+
+//console.log out the response
+console.log(mpg);
+
+//Mary's car gets 34 miles to the gallon
+
+var mpg = 34;
+//console.log out the response
+console.log(34);
+
 
