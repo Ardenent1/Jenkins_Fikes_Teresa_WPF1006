ee0f71f4aac36d58925f62d0950313ce294f3ab7
Teresa Fikes <Ardenent1@gmail.com>
2014-10-12 21:59:04 -0500

    Wacky
    This is my Wacky assignment

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Wacky/Wacky.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Wacky/Wacky.js
@@ -6,4 +6,18 @@
 
 //array-Halloween Candy
 
+prompt(Halloween Candy Choices)
+var Candy = new Array( "Hersey", "Nestle", "Mars", "Taffy", "Homemade", "Toffee");
+console.log(candy);
+Hersey[0] = "Hersey"
+Nestle[1] = "Nestle"
+Mars[2] = "Mars"
+Taffy[4] = "Taffy"
+Homemade[5] = "Homemade"
+Toffee[6] = "Toffee"
+
+document.write("Hersey is "+candy[0])
+console.log(write);
+
+
 
