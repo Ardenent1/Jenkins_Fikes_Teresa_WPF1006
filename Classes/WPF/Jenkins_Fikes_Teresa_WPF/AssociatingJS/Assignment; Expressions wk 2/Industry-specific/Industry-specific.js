23c8fc0e7a5ba905419819be295b4da5f5b0c9ed
Teresa Fikes <Ardenent1@gmail.com>
2014-10-13 14:02:17 -0500

    Industry Specific
    Still working on the Alerts and prompts

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Industry-specific/Industry-specific.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Industry-specific/Industry-specific.js
@@ -5,16 +5,18 @@
  */
 
 //Prompt and Alert
-Var result = prompt("Graphic Artist use Geometry in web design, so let us see what the area of a triangle is");
+var result = prompt("Graphic Artist use Geometry in web design, so let us see what the area of a triangle is");
 
 
 //Triangle formula
 
-var base=prompt("We are calculating the area of a triangle. \nPlease enter the base");
+var base = prompt("We are calculating the area of a triangle. \nPlease enter the base");
 var verticalHeight = prompt("Please enter the height.");
-var area = 1/2 * b * h; //calculates with that info
+var area = 1/2 * base * verticalHeight; //calculates with that info
 //console.log(area); //prints it out to console
 
 var resultTwo =("The area of your rectangle is " + area + " sq feet");
 
 alert(resultTwo);
+
+var area = 1/2 * base * verticalHeight; //calculates with that info
