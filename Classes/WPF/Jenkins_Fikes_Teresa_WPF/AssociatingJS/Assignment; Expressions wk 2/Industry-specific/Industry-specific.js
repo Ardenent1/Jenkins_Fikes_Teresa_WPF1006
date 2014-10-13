e9d19a14bb9af3d752c7fd15cabcc0935358c988
Teresa Fikes <Ardenent1@gmail.com>
2014-10-12 20:32:52 -0500

    Industry-Specific-Triangle1
    Industry-Specific-Triangle The graphic artist use Geometry

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Industry-specific/Industry-specific.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Assignment; Expressions wk 2/Industry-specific/Industry-specific.js
@@ -5,14 +5,16 @@
  */
 
 //Prompt and Alert
-alert(Graphic Artist use Geometry in their designs, so let us see what the area of a rectangle is');
+Var result = prompt("Graphic Artist use Geometry in web design, so let us see what the area of a triangle is");
 
 
 //Triangle formula
 
-var base =prompt("We are calculating the area of a triangle. \nPlease enter the base");
-var verticlHeight = prompt("Please enter the height.");
+var base=prompt("We are calculating the area of a triangle. \nPlease enter the base");
+var verticalHeight = prompt("Please enter the height.");
 var area = 1/2 * b * h; //calculates with that info
 //console.log(area); //prints it out to console
 
-alert("The area of you rectangle is " + area + " sq feet");
+var resultTwo =("The area of your rectangle is " + area + " sq feet");
+
+alert(resultTwo);
