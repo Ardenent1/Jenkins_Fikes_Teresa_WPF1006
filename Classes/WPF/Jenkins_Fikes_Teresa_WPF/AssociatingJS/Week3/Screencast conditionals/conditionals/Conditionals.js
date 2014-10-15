05254868aa0678f1de0b2b0613b9c49e25b87d74
Teresa Fikes <Ardenent1@gmail.com>
2014-10-15 18:31:35 -0500

    Conditional Logic
    Practicing conditional logic

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
@@ -3,12 +3,11 @@
  WPF 1408 Section 01
  Conditionals Week 3
  */
-
-if(sunny){
-    goToTheBeach();
-    if(warmWater) {
-    }
-
- }else{
-        goToTheMovies();
+//Conditional Logic
+var oldEnough = true;
+//if the child is old enough, print the the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+console.log("what comes after");
