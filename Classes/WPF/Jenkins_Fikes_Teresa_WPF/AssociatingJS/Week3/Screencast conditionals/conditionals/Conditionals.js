2d3302681985f32216192880056c213ddec01ed4
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 08:51:57 -0500

    Logical Operators2
    Still practicing changes and decisions

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
@@ -8,18 +8,12 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
-//if the price of the phone is less our budget
-
-if(iPhonePrice < budget){
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 
 }else{
     console.log("No phone for you!!");
 }
-if(paycheck > 300)[
-    console.log("We can buy the phone!");
-]else[
-    console.log("No phone for you!!");
-]
