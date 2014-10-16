775af4ed34bad583b35a3b35cf05fb2ee741c385
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 08:40:15 -0500

    Logical Operators
    comparing two true and false

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
@@ -4,22 +4,22 @@
  Conditionals Week 3
  */
 
-//Conditional Logic- Relational Expressions
+//Coniditional- Logicals
 
-var kidHeight = 52;
-var mineHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
-//if the child is old enough, print the the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight >mineHeight){
-    //you can ride!
-    console.log("You can ride the coaster!");
-}else if(else if){kidHeight > wParentHeight){
-    //you can ride with a parent present
-    console.log("You can ride, but only with a parent present.");
+//if the price of the phone is less our budget
+
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
+
 }else{
-    //sorry you have growing to do
-    console.log("Sorry kid, you've got some growing to do first!");
-
+    console.log("No phone for you!!");
 }
+if(paycheck > 300)[
+    console.log("We can buy the phone!");
+]else[
+    console.log("No phone for you!!");
+]
