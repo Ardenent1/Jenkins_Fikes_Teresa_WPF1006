311e175b928e9b9765d4b4d323e9ca678bcd7276
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 15:54:42 -0500

    Last Chance for Gas!
    Gas Efficiency of driver trying to make it across the dessert

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -4,22 +4,26 @@
  * WPF 1408 Section 01
  Conditionals Week 3
  */
-//if the room temperature is 32 degrees Fahrenheit, what is the degrees in Celsius?
+//Last Chance for Gas!
+//A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+//Ask the user "What is the mpg of you car?"
 
-function convert(degree) {
-    if (degree == "C") {
-        F = document.getElementById("c").value * 9 / 5 + 32;
-        document.getElementById("f").value = Math.round(F);
-    } else	{
-        C = (document.getElementById("f").value -32) * 5 / 9;
-        document.getElementById("c").value = Math.round(C);
-    }
+function calcMPG(){
+    var mpg;
+    var gaugeReading = %;
+    var gasTankCapacity = gallons;
+    if ((isNan(mpg)) || (isNan(gaugeReading) || (isNan(gasTankCapacity))){
+        console.log("Enter 200 for the mileage distance to the next station.");
+    }else
+    {
+        if (gallons > 0){
+            document.forms[0].mpg.value= ((
+
+
+
+                (mpg>0) && (gaugeReading>0)&& (gasTankCapacity>0)
+        }
+        answer = (mpg-gaugeReading)/gasTankCapacity;{
+        if (prompt (“Yes, you can make it without stopping for gas!”){
+    else if} (“You only have X gallons of gas in your tank, better get gas now while you can!”)
 }
-</script>
-</head>
-
-<body>
-<p>Insert a number into one of the input fields below:</p>
-<input id="c" onkeyup="convert('C')"> degrees Celsius<br>
-equals<br>
-<input id="f" onkeyup="convert('F')"> degrees Fahrenheit eyup="convert('F')"> degrees Fahrenheit
