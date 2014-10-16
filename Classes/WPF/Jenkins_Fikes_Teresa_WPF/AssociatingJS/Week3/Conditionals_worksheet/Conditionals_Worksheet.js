ef9bbe00a1e79a43845f27cf0ff4380150ef6edf
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 11:17:52 -0500

    Conditionals Worksheet Assignment
    This is the worksheet assignment for week 3

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -0,0 +1,38 @@
+/**
+ * Teresa Jenkins Fikes
+ * 10/16/14.
+ * WPF 1408 Section 01
+ Conditionals Week 3
+ */
+//if the room temperature is 32 degrees Fahrenheit, what is the degrees in Celsius?
+
+
+
+
+var degFrahren =Number(prompt ("Enter the degrees Fahrenheit",32));
+var degCent;
+
+degCent = 5/9 * (degFahren _32);
+console.log(degFahren + "\xBO Fehrenheit is " + degCent + "/xBO centigrade<br />");
+
+if (degCent < 0){
+    console.log ("The temperature is 0 degrees Celsius that's below the freezing point of water");
+
+}
+if (degCent == 100)
+    console.log("That's the boiling point of water");
+
+
+Degrees = (in F or C);
+Unit = (a string holding an "F" or a "C");
+
+
+Result = "The temperature is X degrees Celsius." Or "The temperature is X degrees Fahrenheit.";
+
+Data Sets to Test; (Note that data sets are not the only numbers that should work with your code.)
+
+32F is 0C
+100C is 212
+F90F is 32.22C
+
+
