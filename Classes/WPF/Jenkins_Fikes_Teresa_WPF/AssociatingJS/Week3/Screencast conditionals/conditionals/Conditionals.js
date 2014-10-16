81252d0d2d4894f75b1668157993d8ec3e716159
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 07:46:45 -0500

    Relational Expressions
    Comparing objects for decision making

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
@@ -3,11 +3,23 @@
  WPF 1408 Section 01
  Conditionals Week 3
  */
-//Conditional Logic
-var oldEnough = true;
+
+//Conditional Logic- Relational Expressions
+
+var kidHeight = 52;
+var mineHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
 //if the child is old enough, print the the console "you can ride!"
-if(oldEnough){
-    //code performed if condition is true
+//if the kid is over 48 inches in height
+if(kidHeight >mineHeight){
+    //you can ride!
     console.log("You can ride the coaster!");
+}else if(else if){kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do first!");
+
 }
-console.log("what comes after");
