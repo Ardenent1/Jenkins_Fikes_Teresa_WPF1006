19198e8da47865e7ef7c0c5c9921b462891b344b
Teresa Fikes <Ardenent1@gmail.com>
2014-10-16 09:14:12 -0500

    Ternary Operators
    Still working with practicing ternaries

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Screencast conditionals/conditionals/Conditionals.js
@@ -6,13 +6,8 @@
 
 //Coniditional Logic- Ternary Operators
 
-var gpa = 48;
-
-//iff the gpa is over the min 2.0 score, the student can graduate
-//*if( gpa > 2.0){
-    console.log("You can graduate!");
-}else{
-    console.log("GPA is too low!");
-}*/
-
-(gpa > 2.0)? console.log("You graduate!") : console.log("GPA to low!");
+var age = 11;
+var book;
+//if the child is under 10, they get green eggs and ham otherwise they get the time machine
+book =  (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
