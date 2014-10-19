7e190d358b7300fc3af0f3ba664c9b3184cd4302
Teresa Fikes <Ardenent1@gmail.com>
2014-10-18 21:11:01 -0500

    Movie Ticket Price
    Determine which two prices the customer is eligible for

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -2,32 +2,18 @@
  * Teresa Jenkins Fikes
  * 10/16/14.
  * WPF 1408 Section 01
- Conditionals Week 3-Tire Pressure
+ Conditionals Week 3-Movie Ticket Price
  */
-//
-Alarm = function() {
+//Ticket Movie Prices
 
-    var lowPressureTreshold = 17;
-    var highPressureTreshold = 21;
-    var sensor = new Sensor();
-    var alarmOn = false;
-};
 
-Alarm.prototype = {
 
-    check: function () {
+    function printPrice() {
+        var movie = document.getElementById("movie").value;
+        var name = document.getElementById("textbox").value;
+        var Cquantity = document.getElementById("Cquantity").value * 1;
+        var Aquantity = document.getElementById("Aquantity").value * 1;
+        var total = 0;
+        var sentence = ("The ticket price is + "X");
+        var price = document.getElementById('Ctickets').value * Cquantity;
 
-        var psiPressureValue = this._sensor.popNextPressurePsiValue();
-        console.log("The tires pass spec!")
-
-        if (psiPressureValue < this._lowPressureTreshold || this._highPressureTreshold < psiPressureValue)
-        {
-            this._alarmOn = true;
-        }
-    },
-
-    alarmOn: function () {
-        console.log("Get your tires checked out!");
-    }
-
-};
