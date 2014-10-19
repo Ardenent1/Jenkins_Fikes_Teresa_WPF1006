f5fcebc1563f38d245f7765124a0ed614a73d609
Teresa Fikes <Ardenent1@gmail.com>
2014-10-18 20:04:40 -0500

    Login
    Still working on the login code

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -9,14 +9,21 @@
 var Username = prompt("Type in your username");
 var Password = Prompt("Type in your password");
 
-}
 //if the username and password is not correct
-var usernameIncorrect=
+if(usernameIncorrect) {
+    UserNotFound;
+}
+}else{
+    "Try Again!"}
+}
+
  //The characters in a password field are masked (shown as asterisks or circles).</p>
 
  var concatString = (“Welcome, + "name" + !”);{if the username and password is correct.{
     {else{Prompt=(“User not found. Try again.”);
     console.log("User not found. Try again.");
 }
-“Password does not match our records.”)  -if the username matches but the password does not
+“Password does not match our records.”);{
+    }else if{
+    }  -if the username matches but the password does not
 
