c51a2880084da32c9d60f2b2eb91468ff7dbb28e
Teresa Fikes <Ardenent1@gmail.com>
2014-10-18 19:52:29 -0500

    Login Checking
    Checking the login

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -4,40 +4,19 @@
  * WPF 1408 Section 01
  Conditionals Week 3
  */
-<form action="">
-Username: <input type="text" name="user"><br>
-Password: <input type="password" name="password">
-    </form>
+//Ask the user to type in their username and password
 
-    <p><b>Note:</b> The characters in a password field are masked (shown as asterisks or circles).</p>
+var Username = prompt("Type in your username");
+var Password = Prompt("Type in your password");
 
-    var concatString = (“Welcome, + "name" + !”);{
-    else{Prompt= (“User not found. Try again.”
+}
+//if the username and password is not correct
+var usernameIncorrect=
+ //The characters in a password field are masked (shown as asterisks or circles).</p>
 
-    }
-    }
-//if the username and password is correct
-
-  -if the username does not match
+ var concatString = (“Welcome, + "name" + !”);{if the username and password is correct.{
+    {else{Prompt=(“User not found. Try again.”);
+    console.log("User not found. Try again.");
+}
 “Password does not match our records.”)  -if the username matches but the password does not
 
-</body>
-</html>
-
-
-
-
-
-Check the Login
-
-Make sure the user has the correct username and password. If the username doesn’t match then a specific message for that should be printed to the console. If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
-
-    Given:
-Username entered by user
-Password entered by user
-Correct username
-Correct password
-Result To Print Out:
-    “Welcome, (place their username here)!”  - if the username and password is correct
-“User not found. Try again.” -if the username does not match
-“Password does not match our records.”  -if the username matches but the password does not
