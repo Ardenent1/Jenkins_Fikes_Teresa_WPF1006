ebdc720edc9f2866e31b28d1c0fbc1839e91c071
Teresa Fikes <Ardenent1@gmail.com>
2014-10-19 20:04:36 -0500

    A Website Poll
    Still editing the website poll

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/conditional_poll.js
@@ -0,0 +1,23 @@
+/**
+ * Teresa Jenkins Fikes
+ * 10/16/14.
+ * WPF 1408 Section 01
+ Conditionals Week 3-Movie Ticket Price
+ */
+//A poll for your website that sends the answer to your email
+
+if (prompt=="");
+    x="unknown";
+else
+    x=document.referrer;
+var inform="From url: "+x+" using browser: "+navigator.appName+" "+navigator.appVersion;
+//-->
+    </script>
+<table border="0" cellpadding="0" cellspacing="0" width="230" bgcolor="#F6CED8">
+    <tr>
+        <td align="center" valign="top"><font size="8" face="New Times Roman">Poll </font></td>
+    </tr>
+</table>
+<table border="0" width="230">
+    <tr>
+        <td><font face="Times New Roman"><strong>Do you think this class is fun?</strong></font><form
