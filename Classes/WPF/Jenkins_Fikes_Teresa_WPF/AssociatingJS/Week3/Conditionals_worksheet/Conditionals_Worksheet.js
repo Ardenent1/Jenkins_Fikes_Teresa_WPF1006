159041284f63790861c38f5bdc3c8dd372e818d3
Teresa Fikes <Ardenent1@gmail.com>
2014-10-19 19:50:08 -0500

    Website Poll
    A poll for a website

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_worksheet/Conditionals_Worksheet.js
@@ -4,33 +4,9 @@
  * WPF 1408 Section 01
  Conditionals Week 3-Movie Ticket Price
  */
-//Ticket Movie Prices
+//A poll for your website that sends the answer to your email
 
-
-
-    function printPrice() {
-        var movie = document.getElementById("movie").value;
-        var name = document.getElementById("textbox").value;
-        var Cquantity = document.getElementById("Cquantity").value * 1;
-        var Aquantity = document.getElementById("Aquantity").value * 1;
-        var total = 0;
-        var sentence = ("The ticket price is + "X");
-        var price = document.getElementById('Ctickets').value * Cquantity;
-
-    sentence += name + "<br>purchased "+Cquantity+" Child ticket(s) to \"";
-    sentence += movie + "\" for $";
-    sentence += price;
-    total += price;
-
-    price = document.getElementById('Atickets').value * Aquantity;
-    sentence += "<br>and "+Aquantity+" Adult ticket(s) to \"";
-    sentence += movie + "\" for $";
-    sentence += price +'<br>';
-    total += price;
-
-    sentence += "<br>";
-    sentence += 'Total cost: $'+total;
-    console.log("inserts").innerHTML=sentence;
-}
-
-
+if (prompt=="")
+    x="unknown"
+else
+    x=document.referrer;
