a73d2890e74f2639a9adc2f038c2bcf260c3d534
Teresa Fikes <Ardenent1@gmail.com>
2014-10-19 22:03:52 -0500

    My Grandmas Age
    My Grandmas Age is calculated

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/My Grandmas Age/My_Grandmas_Age.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/My Grandmas Age/My_Grandmas_Age.js
@@ -18,3 +18,15 @@ if ( ! (myGrandmasAge >= 0 && myGrandmasAge <= 70) );
 {
     console.log (“myGrandmasAge is not between 0 and 70<br />”);
     }
+if ( myGrandmasAge >= 80 || myGrandmasAge <= 70 );
+{
+    console.log(“myGrandmasAge is 80 or above OR  70 or below<br />”) )
+    {
+if ( (myGrandmasAge >=  60 && myGrandmasAge <= 69) || (myGrandmasAge >= 60 && myGrandmasAge <= 69) )
+    }
+    console.log(“myGrandmasAge is between 60 and 69 or MyGrandmasAge is between 80 and 89”);
+    }
+        console.log(“myGrandmasAge is NOT between 0 and 70, “ = “nor is it between 30 and 39, nor is it between 80 and 89”);
+    }
+
+
