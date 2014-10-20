daf1b4a638f32e43184bb26f1a76ab931402737a
Teresa Fikes <Ardenent1@gmail.com>
2014-10-19 20:18:43 -0500

    A Website Poll
    Still a website poll

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/conditional_poll.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/conditional_poll.js
@@ -7,28 +7,38 @@
 //A poll for your website that sends the answer to your email
 
 if (prompt=="");
-    x="unknown";
+    x="unknown";}
 else
+}
     x=document.referrer;
 var inform="From url: "+x+" using browser: "+navigator.appName+" "+navigator.appVersion;
 //-->
-    </script>
-<table border="0" cellpadding="0" cellspacing="0" width="230" bgcolor="#F6CED8">
-    <tr>
-        <td align="center" valign="top"><font size="6" face="New Times Roman">Poll</font></td>
-    </tr>
-</table>
-<table border="0" width="230">
-    <tr>
-        <td><font face="Times New Roman"><strong>Do you think this class is fun?</strong></font><form
-        console.log('<input type="hidden" name="info"
-            value='+'"'+inform+'">');
-            //-->
-            </script></td>
-        </tr>
+</script>
+    <table border="0" cellpadding="0" cellspacing="0" width="230" bgcolor="#F6CED8">
         <tr>
-            <td valign="top" width="30%"><font size="6" face="New Times Roman">No</font></td>
-            <td align="right" valign="top" width="30%"><input type="radio" name="response" value="no"><br>
-            </td>
-                <td align="right" width="40%"><input type="submit" name="B1" value="Submit!"
-                style="background-color:pink"></td>
+            <td align="center" valign="top"><font size="6" face="New Times Roman">Poll</font></td>
+        </tr>
+    </table>
+    <table border="0" width="230">
+        <tr>
+            <td><font face="Times New Roman"><strong>Do you think this class is fun?</strong></font><form
+            action="mailto:ardenentent1@gmail.com?subject=poll results" method="POST" enctype="text/plain"
+            name="poll">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td valign="top" width="30%"><font size="6" face="New Times Roman">Yes</font></td>
+                        <td align="right" valign="top" width="30%"><input type="radio" checked name="response"
+                        value="yes"></td>
+                            <td align="right" width="40%"><script>
+                                <!--
+                                console.log('<input type="hidden" name="info" value='+'"'+inform+'">')
+                                //-->
+                            </script></td>
+                        </tr>
+                        <tr>
+                            <td valign="top" width="30%"><font size="6" face="New Times Roman">No</font></td>
+                            <td align="right" valign="top" width="30%"><input type="radio" name="response" value="no"><br>
+                            </td>
+                                <td align="right" width="40%"><input type="submit" name="B1" value="Submit!"
+                                style="background-color:pink"></td>
+
