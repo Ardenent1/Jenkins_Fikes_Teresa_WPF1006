dbfbbedf36432b48b17a0115972086b6bbde6c94
Teresa Fikes <Ardenent1@gmail.com>
2014-10-19 20:07:08 -0500

    Poll
    A website poll

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/conditional_poll.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/Week3/Conditionals_Personal wk3/conditional_poll.js
@@ -21,4 +21,14 @@ var inform="From url: "+x+" using browser: "+navigator.appName+" "+navigator.app
 <table border="0" width="230">
     <tr>
         <td><font face="Times New Roman"><strong>Do you think this class is fun?</strong></font><form
-        console.log('<input type="hidden" name="info" 
+        console.log('<input type="hidden" name="info"
+            value='+'"'+inform+'">')
+            //-->
+            </script></td>
+        </tr>
+        <tr>
+            <td valign="top" width="30%"><font size="6" face="New Times Roman">No</font></td>
+            <td align="right" valign="top" width="30%"><input type="radio" name="response" value="no"><br>
+            </td>
+                <td align="right" width="40%"><input type="submit" name="B1" value="Submit!"
+                style="background-color:yellow"></td>
