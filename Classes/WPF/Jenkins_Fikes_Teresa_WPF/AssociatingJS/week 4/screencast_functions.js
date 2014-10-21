3ad48c68d85d65c97d70884d3f11aba1d8260e51
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 18:51:49 -0500

    Screencast Function Invocation
    How to make a function happen

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
@@ -4,16 +4,18 @@
  * Screencast Functions week 4
  * 10/21/14.
  */
-
-//Functions - Basic Structure
+//Functions - Function Execution
 
 function outptMsg(){
     console.log("Hello World");
 }
 
-functuin calcArea(){
+function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+calcArea();
+calcArea();
+calcArea();
