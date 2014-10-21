f9740df49f56df0841be615c303974f358a0d537
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 18:39:07 -0500

    Screencast Functions
    Basic Structure in basic functions

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
@@ -10,3 +10,10 @@
 function outptMsg(){
     console.log("Hello World");
 }
+
+functuin calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
