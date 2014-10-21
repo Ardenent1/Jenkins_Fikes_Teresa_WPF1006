809e26a82ef722d5867b50f44abc97aa3c2c172e
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 17:59:48 -0500

    Screencast Functions
    Differences in functions

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
@@ -0,0 +1,12 @@
+/**
+ * Teresa Jenkins Fikes
+ * WPF 1408 Section 01
+ * Screencast Functions week 4
+ * 10/21/14.
+ */
+
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
