6e9ea9b95b667b92853ed0202b671e7c9bdcf91d
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 22:10:36 -0500

    While loop
    Description of a while loop

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/Screencast Loops/Screencast_Loops.js
@@ -0,0 +1,30 @@
+/**
+ *Teresa Jenkins Fikes
+ *  WPF 1408 Section 01
+ * Screencast Functions week 4
+ * 10/21/14.
+ */
+
+//Screencast - While Loop
+
+1 console.log('------Loops------')
+2
+3 var b = 50;//setups the index
+4
+5 while (b > 0)[//checks the condition
+6    console.log(b + ' kegs on the wall');
+7    b--;//increments or decrements the index
+8 }
+10
+11
+12
+13
+12
+13
+14
+15
+16
+17
+18
+19
+20
