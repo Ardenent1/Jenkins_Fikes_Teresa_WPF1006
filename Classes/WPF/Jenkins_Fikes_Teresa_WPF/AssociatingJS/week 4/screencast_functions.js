db0a1c54ad81b0aa6bcdb025ddb0a6d2aa300420
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 19:37:37 -0500

    Screencast Functions Parameters and Arguments
    Screencast Functions Parameters and Arguments

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
@@ -6,10 +6,10 @@
  */
 //Functions - Arguments and Parameters
 
-calcArea(30, 20);
-
-function calcArea(w, h){//w=30, h=20
-
-    var area = w * h;
-    console.log(area);
+function dogYears(age){//parameters
+    var dogYears = age * 7;
+    console.log("Raven is " + dogYears + "years pld.");
 }
+var age1 = 4;
+dogYears(age1);//arguments
+dogYears(5);
