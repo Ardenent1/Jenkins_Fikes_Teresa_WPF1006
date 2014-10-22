6564742fc75f57e38acf91f77d712ff9ada174bb
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 19:22:42 -0500

    Screencast Arguments and Parameters
    Screencast Arguments and Parameters

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/screencast_functions.js
@@ -4,16 +4,5 @@
  * Screencast Functions week 4
  * 10/21/14.
  */
-//Functions - Variable Scope
+//Functions - Arguments and Parameters
 
-var width = 5;
-}
-
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
-    console.log(area);
-}
-calcArea();
-console.log(width);
