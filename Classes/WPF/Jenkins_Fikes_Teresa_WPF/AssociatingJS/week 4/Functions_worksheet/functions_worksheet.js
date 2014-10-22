bad41351db80de01d66fec9877b146f1f1837b2e
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 21:22:23 -0500

    Stung!
    Calculate Bee stings needed to kill an animal

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/functions_worksheet.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/functions_worksheet.js
@@ -5,26 +5,17 @@
  * 10/21/14.
  */
 
+//Stung!
 
-//Calculate the Circumference of a circle
+//It takes 8.666666667  to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
 
-var diameter = 2;
+function functionName = (weightInPounds)[
+    var victimsWeight = weightInPounds * 8.666666667;
+    console.log("It takes " + victimsWeight + " bee stings to kill this animal.")
+]
+victimsWeight(weightInPounds);
 
-var Circumference = calcCircumference(2 * (Math.PI * rad))
 
-//This is the function that performs the calculation:
 
-//it takes the radius as input and returns the circumference
-
-function calcCircumference(rad)
-
-{
-
-    var circumference = 2 * (Math.PI * rad);
-
-    return circumference;
-
-}
-console.log("The circumference of the circle is X")
 
 
