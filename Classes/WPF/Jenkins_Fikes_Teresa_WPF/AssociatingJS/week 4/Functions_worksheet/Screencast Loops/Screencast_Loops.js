675c0f2d1d6cd4b167737be2bf3fa86d001f5c36
Teresa Fikes <Ardenent1@gmail.com>
2014-10-22 09:17:11 -0500

    Do While Loops
    How to run Do while Loops

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/Screencast Loops/Screencast_Loops.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/Screencast Loops/Screencast_Loops.js
@@ -5,26 +5,21 @@
  * 10/21/14.
  */
 
-//Screencast - While Loop
+//Screencast - do while loop
 
-1 console.log('------Loops------')
-2
-3 var b = 50;//setups the index
-4
-5 while (b > 0)[//checks the condition
-6    console.log(b + ' kegs on the wall');
-7    b--;//increments or decrements the index
-8 }
-10
-11
-12
-13
-12
-13
-14
-15
-16
-17
-18
-19
-20
+console.log('------Loops------')
+var b = 50;//setups the index
+
+while (b > 0)[//checks the condition
+    console.log(b + ' kegs on the wall');
+    b--;//increments or decrements the index
+}
+
+console.log('---Do While Loops----');
+
+var c = 10;
+do{
+-------console.log(c + ' kegs on the wall');
+-------c--;
+}
+while(c> 0);
