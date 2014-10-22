9fade3161b8c22dc874bb5bb583ccad3a969d2b4
Teresa Fikes <Ardenent1@gmail.com>
2014-10-21 20:58:40 -0500

    Circumfernce
    Function for calculating the circumference

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Functions_worksheet/functions_worksheet.js
@@ -0,0 +1,30 @@
+/**
+ * Teresa Jenkins Fikes
+ *  WPF 1408 Section 01
+ * Screencast Functions week 4
+ * 10/21/14.
+ */
+
+
+//Calculate the Circumference of a circle
+
+var diameter = 2;
+
+var Circumference = calcCircumference(2 * (Math.PI * rad))
+
+//This is the function that performs the calculation:
+
+//it takes the radius as input and returns the circumference
+
+function calcCircumference(rad)
+
+{
+
+    var circumference = 2 * (Math.PI * rad);
+
+    return circumference;
+
+}
+console.log("The circumference of the circle is X")
+
+
