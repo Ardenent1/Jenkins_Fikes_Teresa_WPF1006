07d85099badf45aa5dfa94cf49858987ce372654
Teresa Fikes <Ardenent1@gmail.com>
2014-10-23 18:33:44 -0500

    Week 4 Personal
    This is the personal assignment for week 4.

---
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Assignment Functions/Personal/personal.js
@@ -0,0 +1,26 @@
+/**
+ * Teresa Jenkins Fikes
+ * 10/23/14.
+ * WPF 1408 Section 01
+ * Week 4
+ */
+
+//User wants to know how many candy bags they will have left after giving one bag to each student in the class
+
+
+var numStudents = prompt("How many students are in your class? \n Please enter the number amount");
+console.log(numStudents);
+
+var numCandyBags = prompt("How many candy bags do you have? \n Please enter the number amount");
+
+
+function subtractNumbers(num1, num2){
+    return num1 - num2;//just return the difference of the two number arguments
+}
+console.log(num1, num2);
+
+
+
+
+
+
