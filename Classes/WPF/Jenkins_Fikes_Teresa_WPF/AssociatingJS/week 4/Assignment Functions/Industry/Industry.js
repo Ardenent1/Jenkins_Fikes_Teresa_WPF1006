68a9229394dbcf0acca31f324891533ae299c425
Teresa Fikes <Ardenent1@gmail.com>
2014-10-23 20:39:17 -0500

    Industry Assignment-Week 4
    The first comment for Industry week 4 setting up

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Assignment Functions/Industry/Industry.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Assignment Functions/Industry/Industry.js
@@ -5,3 +5,15 @@
  * Week 4-Industry Assignment
  */
 
+// Create a function that takes two arguments and returns the sum of those arguments
+//Web Design Assignment is ordered
+//Design rate is $800
+//writing rate is $200 for 5 post
+
+var designRate = a;
+var writingRate = b;
+var calc = new Function('a', 'b', 'return a + b');
+
+// Call the function
+result(2, 6);
+// > 8
