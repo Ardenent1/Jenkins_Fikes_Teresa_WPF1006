83c09070341102db7627e64c054db9ae288810d3
Teresa Fikes <Ardenent1@gmail.com>
2014-10-23 19:16:21 -0500

    Personal Assignment wk 4
    This is the personal assignment for week 4 with the “if else”

--- Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Assignment Functions/Personal/personal.js
+++ Classes/WPF/Jenkins_Fikes_Teresa_WPF/AssociatingJS/week 4/Assignment Functions/Personal/personal.js
@@ -2,22 +2,26 @@
  * Teresa Jenkins Fikes
  * 10/23/14.
  * WPF 1408 Section 01
- * Week 4
+ * Week 4-Personal Assignment
  */
 
 //User wants to know how many candy bags they will have left after giving one bag to each student in the class
 
 
 var numStudents = prompt("How many students are in your class? \n Please enter the number amount");
-console.log(numStudents);
 
-var numCandyBags = prompt("How many candy bags do you have? \n Please enter the number amount");
-
+var numCandyBags = prompt("How many candy bags do you have? \n Please enter the number amount"){
+if(numStudents > numCandyBags){
+//code perform if condition is true
+    console.log("You have enough candy Bags!")
+}else{
+//code performed if condition is false
+    console.log("You need more candy bags!");
+}
 
 function subtractNumbers(num1, num2){
-    return num1 - num2;//just return the difference of the two number arguments
+        return num1 - num2;//just return the difference of the two number arguments
 }
-console.log(num1, num2);
 
 
 
